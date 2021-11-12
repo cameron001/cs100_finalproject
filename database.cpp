1ad807f69702fa0c928c61135605d145a0dd9e7b
w3mona <4096344+w3mona@users.noreply.github.com>
2021-11-12 08:55:13 -0800

    Missing database class

---
+++ database.cpp
@@ -0,0 +1,35 @@
+#include "database.h"
+
+Database* Database::instance = nullptr;
+
+Database::Database() : QSqlDatabase(addDatabase("QSQLITE"))
+{
+    this->setDatabaseName(qApp->applicationDirPath()
+                          + QDir::separator()
+                          + "Highlander_books.db");
+    QFileInfo databaseInfo(qApp->applicationDirPath()
+                          + QDir::separator()
+                          + "Highlander_books.db");
+    qDebug() << "database path:" << qApp->applicationDirPath()
+                              + QDir::separator()
+                              + "Highlander_books.db";
+   if (databaseInfo.exists())
+   {
+        qDebug() << "Database file is open.\n";
+        this->open();
+        QSqlQuery query;
+        query.prepare("PRAGMA foreign_keys = ON");
+        query.exec();
+    }
+    else
+    {
+        qDebug() << "Database file is not open.\n";
+    }
+}
+
+Database* Database::getInstance()
+{
+    if(instance == nullptr)
+        instance = new Database;
+     return instance;
+}
