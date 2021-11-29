2dc2d91288839df1f97992f6d238069667beb83f
Blake Dickerson <dickersonb27@yahoo.com>
2021-11-28 20:11:53 -0800

    added more db tests

--- UnitTesting/instanceTest/tst_dbinstancetest.cpp
+++ UnitTesting/instanceTest/tst_dbinstancetest.cpp
@@ -2,6 +2,7 @@
 #include <QCoreApplication>
 #include "../database.h"
 // add necessary includes here
+#include <QSqlDriver>
 
 class dbInstanceTest : public QObject
 {
@@ -11,6 +12,10 @@ class dbInstanceTest : public QObject
 
 private slots:
     void test_case1();
+    void openDb();
+    void closeDb();
+    void testConnectName();
+    void testFakeConnectName();
 
 };
 
@@ -20,6 +25,50 @@ void dbInstanceTest::test_case1()
 {
     Database* testdb = Database::getInstance();
     QVERIFY2(testdb != nullptr, "database instance wasn't created");
+}
+
+void dbInstanceTest::openDb()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", "first");
+    db.setDatabaseName(qApp->applicationDirPath()
+                       + QDir::separator()
+                       + "Highlander_books.db");
+    db.close();
+    db.removeDatabase(qApp->applicationDirPath()
+                      + QDir::separator()
+                      + "Highlander_books.db");
+    QCOMPARE(db.open(), true);
+}
+
+void dbInstanceTest::closeDb()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", "second");
+    db.setDatabaseName(qApp->applicationDirPath()
+                       + QDir::separator()
+                       + "Highlander_books.db");
+    if (db.open())
+    {
+        db.close();
+    }
+    db.removeDatabase(qApp->applicationDirPath()
+                      + QDir::separator()
+                      + "Highlander_books.db");
+    QCOMPARE(!db.open(), false);
+}
+
+void dbInstanceTest::testConnectName()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", "first");
+    db.setDatabaseName(qApp->applicationDirPath()
+                       + QDir::separator()
+                       + "Highlander_books.db");
+    QVERIFY(db.isValid());
+}
+
+void dbInstanceTest::testFakeConnectName()
+{
+    QSqlDatabase db;
+    QCOMPARE(db.isValid(), false);
 }
 
 QTEST_MAIN(dbInstanceTest)
