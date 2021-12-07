6cc3f76930f8cf1a3d82bf7a8081fef6ed1c211f
sourshoes <86706385+sourshoes@users.noreply.github.com>
2021-12-06 18:12:40 -0800

    Update facultylogin.h

--- facultylogin.h
+++ facultylogin.h
@@ -1,4 +1,51 @@
 #ifndef FACULTYLOGIN_H
 #define FACULTYLOGIN_H
 
+#include <QDialog>
+
+#include <QScreen>
+#include <QStyle>
+#include <QDebug>
+#include <QFont>
+#include <QValidator>
+#include <QMessageBox>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+
+#include "facultyaccountpage.h"
+using namespace HighlanderBooks;
+
+namespace Ui {
+    class facultyLogin;
+}
+
+class facultyLogin : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit facultyLogin(QWidget* parent = nullptr);
+    ~facultyLogin();
+
+    void centerAndResize();
+
+    void createLoginLabel();
+
+    void createusernameEdit();
+
+   
+    void createpasswordEdit();
+
+    void createLoginButton();
+    HighlanderBooks::user* login(QString username, QString password);
+
+private slots:
+    void on_loginButton_clicked();
+
+private:
+    Ui::facultyLogin* ui;
+    QMap<QString, QMap<QString, HighlanderBooks::user*> > facultyDbCacheLogin;
+};
+
 #endif // FACULTYLOGIN_H
