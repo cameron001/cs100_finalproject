d16dc24159bd53f207a3937cdca97c61fd63a318
sourshoes <86706385+sourshoes@users.noreply.github.com>
2021-12-06 22:26:22 -0800

    Update librarianaccountpage.h

--- librarianaccountpage.h
+++ librarianaccountpage.h
@@ -1,7 +1,9 @@
 #ifndef LIBRARIANACCOUNTPAGE_H
 #define LIBRARIANACCOUNTPAGE_H
+
 #include <QDialog>
 #include <QScreen>
+
 #include "deletebookpage.h"
 #include "searchbyauthorpage.h"
 #include "searchbytitlepage.h"
@@ -9,27 +11,38 @@
 #include "addbookpage.h"
 #include "editbookpage.h"
 #include "displaybooks.h"
+
 namespace Ui {
 class librarianAccountPage;
 }
+
 class librarianAccountPage : public QDialog
 {
     Q_OBJECT
+
 public:
     explicit librarianAccountPage(QWidget *parent = nullptr);
     ~librarianAccountPage();
+
     void centerAndResize();
+
     void createWelcomeLabel();
+
     void createAddBookButton();
+
     void createEditBookButton();
+
     void createDeleteBookButton();
+
     void createSearchByAuthorButton();
+
     void createSearchByISBNButton();
+
     void createSearchByBookTitleButton();
+
     void createDisplayBooksButton();
     void createLogoutbtn();
 void on_searchByISBNButton_clicked();
-void createSearchByISBNButton();
 
 
 
@@ -38,17 +51,21 @@ private slots:
 
     void on_searchByAuthorButton_clicked();
 
-    void on_searchByISBNButton_clicked();
 
 
     void on_searchByBookTitleButton_clicked();
 
     void on_addBookButton_clicked();
+
     void on_editBookButton_clicked();
+
     void on_logout_clicked();
+
     void on_displayBooksButton_clicked();
+
 private:
     Ui::librarianAccountPage *ui;
     QWidget * dailog;
 };
+
 #endif // LIBRARIANACCOUNTPAGE_H
