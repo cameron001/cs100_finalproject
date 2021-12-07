b6a10c975974f6227e4c3816a5dab1a9051b4fa1
sourshoes <86706385+sourshoes@users.noreply.github.com>
2021-12-06 18:17:40 -0800

    Update librarianaccountpage.h

--- librarianaccountpage.h
+++ librarianaccountpage.h
@@ -1,9 +1,7 @@
-#ifndef FACULTYACCOUNTPAGE_H
-#define FACULTYACCOUNTPAGE_H
-
+#ifndef LIBRARIANACCOUNTPAGE_H
+#define LIBRARIANACCOUNTPAGE_H
 #include <QDialog>
 #include <QScreen>
-
 #include "deletebookpage.h"
 #include "searchbyauthorpage.h"
 #include "searchbytitlepage.h"
@@ -11,38 +9,27 @@
 #include "addbookpage.h"
 #include "editbookpage.h"
 #include "displaybooks.h"
-
 namespace Ui {
-    class facultyAccountPage;
+class librarianAccountPage;
 }
-
-class facultyAccountPage : public QDialog
+class librarianAccountPage : public QDialog
 {
     Q_OBJECT
-
 public:
-    explicit facultyAccountPage(QWidget* parent = nullptr);
-    ~facultyAccountPage();
-
+    explicit librarianAccountPage(QWidget *parent = nullptr);
+    ~librarianAccountPage();
     void centerAndResize();
-
     void createWelcomeLabel();
-
     void createAddBookButton();
-
     void createEditBookButton();
-
     void createDeleteBookButton();
-
     void createSearchByAuthorButton();
-
     void createSearchByISBNButton();
-
     void createSearchByBookTitleButton();
-
     void createDisplayBooksButton();
     void createLogoutbtn();
-    void on_searchByISBNButton_clicked();
+void on_searchByISBNButton_clicked();
+void createSearchByISBNButton();
 
 
 
@@ -51,21 +38,17 @@ private slots:
 
     void on_searchByAuthorButton_clicked();
 
+    void on_searchByISBNButton_clicked();
 
 
     void on_searchByBookTitleButton_clicked();
 
     void on_addBookButton_clicked();
-
     void on_editBookButton_clicked();
-
     void on_logout_clicked();
-
     void on_displayBooksButton_clicked();
-
 private:
-    Ui::facultyAccountPage* ui;
-    QWidget* dailog;
+    Ui::librarianAccountPage *ui;
+    QWidget * dailog;
 };
-
-#endif // FACULTYACCOUNTPAGE_H
+#endif // LIBRARIANACCOUNTPAGE_H
